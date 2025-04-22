cc8bcbeb1ecacabb77524c8fa026c5f76c7b1118
Diony-source <erendalkiran2001@gmail.com>
2025-04-22 22:50:16 +0300

    Setup HTTP server and route people endpoints

--- main.go
+++ main.go
@@ -2,20 +2,42 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/joho/godotenv"
 	"github.com/Diony-source/peoplehub-api/utils"
-	"github.com/Diony-source/peoplehub-api/repository"
+	"github.com/Diony-source/peoplehub-api/handlers"
 )
 
 func main() {
 	godotenv.Load()
 	utils.InitDB()
 
-	err := repository.InsertPerson("Alice", 30)
-	if err != nil {
-		log.Fatal("Insert failed:", err)
-	}
+	http.HandleFunc("/people", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			handlers.GetPeopleHandler(w, r)
+		case http.MethodPost:
+			handlers.PostPersonHandler(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
 
-	log.Println("✔ Person inserted.")
+	http.HandleFunc("/people/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodDelete:
+			handlers.DeletePersonHandler(w, r)
+		case http.MethodPatch:
+			handlers.PatchPersonHandler(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.HandleFunc("/people/search", handlers.SearchPeopleHandler)
+	http.HandleFunc("/people/stats", handlers.StatsHandler)
+
+	log.Println("🚀 Server started at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
